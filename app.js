c85effeede55a3ef33dadcf229e86a25198157df
Garth080 <hhgt80@gmail.com>
2025-02-20 15:07:03 -0600

    actualiza el codigo

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = document.getElementById('amigo')
 let amigos = [];
 
 
 function agregarAmigo() {
     // agrega el nombre del amigo
-    let amigo = document.getElementById('amigo');
-    let nombreAmigo = amigo.value;
+        nombreAmigo = amigo.value;
 
     // Verificar que el nombre no esté vacío
     if (nombreAmigo === '') {
         alert('Por favor, escribe un nombre.');
+        return;
+    }
+    //si el nombre exite mostrar mensaje
+    if (amigos.includes(nombreAmigo)) {
+        alert('Este nombre ya está en la lista.');
         return;
     }
 
@@ -22,3 +27,4 @@ function agregarAmigo() {
     // Mostrar un mensaje de éxito
     console.log(`"${nombreAmigo}" ha sido agregado al array.`);
 }
+
