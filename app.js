0d36264b534edf908c4f7172e7add7001c3d7e84
Garth080 <hhgt80@gmail.com>
2025-02-27 15:51:48 -0600

    terminando el codigo

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigo = document.getElementById('amigo')
-let listaAmigos = document.getElementById('listaAmigos');
 let amigos = [];
 
-
 function agregarAmigo() {
+    let amigo = document.getElementById('amigo')
+    let listaAmigos = document.getElementById('listaAmigos');
     // agrega el nombre del amigo
-        nombreAmigo = amigo.value;
+    let  nombreAmigo = amigo.value;
 
     // Verificar que el nombre no esté vacío
     if (nombreAmigo === "") {
         alert('Por favor, escribe un nombre.');
         return;
     }
-    //si el nombre exite mostrar mensaje
-    if (amigos.includes(nombreAmigo)) {
-        alert("Este nombre ya está en la lista.");
-        return;
 
-       
+    //si el nombre ya existe mostrar alerta
+    if (amigos.includes(nombreAmigo)) {
+        alert('¡Este nombre ya está en la lista!');
+        return;
     }
-    //mostrar lista de amigos
-    listaAmigos.innerHTML += `<li>${amigo.value}</li>`
 
     // Agregar el nombre al array 
-    amigos.push(nombreAmigo);
+    amigos.push(nombreAmigo);    
 
-    // Limpiar la caja después de agregar el nombre
-    amigo.value = "";
+     //mostrar lista de amigos
+    listaAmigos.innerHTML += `<li>${amigo.value}</li>`
 
-    // Mostrar un mensaje de éxito en la consola
-    console.log(`"${nombreAmigo}" ha sido agregado al array.`);
+     // Limpiar la caja después de agregar el nombre
+     amigo.value = "";
+     //console.log (amigos)
 
 }
 
+function sortearAmigo() {
+        if (amigos.length === 0) {
+        alert ("no hay amigos para sortear")
+   
+    }    
 
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+}
+
+    
